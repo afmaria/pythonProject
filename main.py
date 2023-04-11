ac7addfac884285f38b0bc796e37ecc08d576678
afmaria <afonicheva.mari@gmail.com>
2023-04-11 21:11:30 +0300

    Задание 3

---
+++ main.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script_name, u_time, salary_by_hour, bonus = argv
+print("Имя скрипта: ", script_name)
+print("Параметр 1: ", u_time)
+print("Параметр 2: ", salary_by_hour)
+print("Параметр 3: ", bonus)
